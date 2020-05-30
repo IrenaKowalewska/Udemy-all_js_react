25944f0c7fd4ae8ad967e94ec1593d155778d79b
kleo4004 <zkioz4004@gmail.com>
2020-05-30 22:51:31 +0300

    feat:add movie-questions

--- movie_number/js/script.js
+++ movie_number/js/script.js
@@ -10,11 +10,24 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b  = +(prompt('На сколько его оцените?', ''));
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d  = +(prompt('На сколько его оцените?', ''));
+for (let i = 0; i < 2; i++) {
+    const nameFilm = prompt('Один из последних просмотренных фильмов?', '');
+    const rating  = prompt('На сколько его оцените?', '');
+    if (nameFilm != null && rating != null && nameFilm != '' && rating != 0 && nameFilm.length < 50) {
+        personalMovieDB.movies[nameFilm] = rating;
+    } else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB)
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов.');
+} else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
+    alert('Вы классический зритель.');
+} else if (30 <= personalMovieDB.count) {
+    alert('Вы киноман!');
+} else {
+    alert('Произошла ошибка!')
+}
+
+console.log(personalMovieDB);
